5d8a7c393aeda42d9ea6771d4a879808f406dd7b
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-12-23 19:46:19 +0100

    aggiunto eureka client

--- src/main/java/com/soprasteria/bookservice/BookServiceApplication.java
+++ src/main/java/com/soprasteria/bookservice/BookServiceApplication.java
@@ -3,6 +3,7 @@ package com.soprasteria.bookservice;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.soprasteria.bookservice.model.Book;
 
 @SpringBootApplication
@@ -18,6 +21,15 @@ import com.soprasteria.bookservice.model.Book;
 @RestController
 @RequestMapping("")
 public class BookServiceApplication {
+	
+	@Autowired
+	private EurekaClient discoveryClient;
+	
+	public String serviceUrl() {
+	    InstanceInfo instance = discoveryClient.getNextServerFromEureka("rating-service", false);
+	    String ratingServiceUrl = instance.getHomePageUrl();
+	    return ratingServiceUrl;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
@@ -33,6 +45,7 @@ public class BookServiceApplication {
 
 	@GetMapping("/{bookId}")
 	public Book findBook(@PathVariable Long bookId) {
+		System.out.println(serviceUrl());
 		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
 	}
 
